75652fdb87ce083271acb8a39e85a4afa8999124
minh_laptop <ldm20042000@gmail.com>
2023-10-19 17:35:40 +0700

    second

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  // Thực hiện bất kỳ xử lý nào dành cho toàn bộ ứng dụng ở đây
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
